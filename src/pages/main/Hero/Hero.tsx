87049a0769799ae96d7a72383028b65cf67fb73b
Pedro Miguel <87210287+Pedro6Miguel@users.noreply.github.com>
2022-12-12 17:14:19 +0000

    Responsiveness update

--- src/pages/main/Hero/Hero.tsx
+++ src/pages/main/Hero/Hero.tsx
@@ -12,12 +12,13 @@ import DownloadIcon from '/DownloadIcon.png'
 import LogoIcon from '/LogoIcon.png'
 import Tournaments from '/Tournaments.png'
 
-import { UilAngleDown, UilUser } from '@iconscout/react-unicons'
+import { UilAngleDown, UilUser, UilBars, UilTimes } from '@iconscout/react-unicons'
 
 function Hero() {
 
   const [open, setOpen] = useState(false)
   const [esportsOpen, setEsportsOpen] = useState(false)
+  const [navOpen, setNavOpen] = useState(false)
 
   // -- //
 
@@ -33,10 +34,10 @@ function Hero() {
         <img src={sliderData.imge} alt="" className='relative w-full h-[736px] object-cover'/>
         <div id='lightning' className='absolute top-0 left-0'></div>
 
-        <nav className={esportsOpen || open ? 'absolute top-0 left-0 flex w-full h-28 items-center border-b-[2px] border-nav bg-dropnav' : 'absolute top-0 left-0 flex w-full h-28 items-center border-b-[2px] border-nav'}>
+        <nav className={esportsOpen || open || navOpen ? 'absolute top-0 left-0 flex w-full h-28 items-center border-b-[2px] border-nav bg-dropnav max-md:justify-between' : 'absolute top-0 left-0 flex w-full h-28 items-center border-b-[2px] border-nav max-md:justify-between'}>
             <div className='flex ml-52 max-lg:ml-28'>
                 <a href=""><img src={BlizzardLogo} alt="" /></a>
-                <ul className='flex gap-10 pl-32 items-center text-white'>
+                <ul className='flex gap-10 pl-32 items-center text-white max-md:hidden'>
                     <li className='flex cursor-pointer' onClick={() => {setOpen(!open); setEsportsOpen(false)}}>Jogos <span className={open ? 'rotate-180 transition-all' : 'transition-all'}>{open ? <UilAngleDown color="#00AEFF"/> : <UilAngleDown />}</span></li>
                     <div id='gradient' className={open ? 'absolute top-[7rem] left-0 h-[540px] w-[100%] z-10 flex justify-center items-center flex-col' : 'hidden'}>
                         <div className='grid grid-cols-6 gap-[100px]'>
@@ -73,11 +74,48 @@ function Hero() {
                     <li className='cursor-pointer'>Suporte</li>
                 </ul>
             </div>
-            <ul className='flex gap-10 absolute right-0 mr-52 text-white max-lg:mr-28'>
+            <ul className='flex gap-10 absolute right-0 mr-52 text-white max-lg:mr-28 max-md:hidden'>
                 <button>Criar conta</button>
                 <button className='flex gap-2 items-center bg-blue border-blue'><UilUser size="20"/> Logar</button>
             </ul>
+            <div onClick={() => {setNavOpen(!navOpen)}} id='burger' className='mr-28 cursor-pointer'>{navOpen ? <UilTimes color="#fff" size={40}/> : <UilBars color="#fff" size={40}/>}</div>
         </nav>
+        <div id='gradient' className={navOpen ? 'text-white absolute top-0 w-[100%] h-[656px] mt-28 z-10 flex flex-col justify-center gap-16 items-center' : 'hidden'}>
+            <li className='flex cursor-pointer' onClick={() => {setOpen(!open); setEsportsOpen(false)}}>Jogos <span className={open ? 'rotate-180 transition-all' : 'transition-all'}>{open ? <UilAngleDown color="#00AEFF"/> : <UilAngleDown />}</span></li>
+                    <div id='gradient' className={open ? 'absolute top-0 left-0 h-[656px] w-[100%] z-10 flex pt-[100px] items-center flex-col' : 'hidden'}>
+                        <div className='grid grid-cols-6 gap-[100px]'>
+                            {GAMES_DROPDOWN.map((data, i) =>(
+                                <div className='flex flex-col items-center gap-[10px] cursor-pointer'>
+                                    <img className='h-[80px] w-[80px]' src={data.image} alt="" />
+                                    <p className='text-text font-normal text-center text-[14px]'>{data.name}</p>
+                                </div>
+                            ))}
+                        </div>
+                        <div className='w-[100%] h-[90px] bg-dropdown absolute bottom-[0] flex items-center justify-center gap-32'>
+                            <li className='flex items-center gap-4 cursor-pointer'><img className='w-[10px] h-[10px]' src={BlueDots} alt="" /> Ver todos jogos</li>
+                            <li className='flex items-center gap-4 cursor-pointer'><img src={LogoIcon} alt="" /> Aplicativo Battle.net</li>
+                            <li className='flex items-center gap-4 cursor-pointer'><img className='w-[22px] h-[22px]' src={DownloadIcon} alt="" /> Downloads</li>
+                            <li className='flex items-center gap-4 cursor-pointer'><img src={Chat} alt="" /> Fóruns dos jogos</li>
+                        </div>
+                    </div>
+                    <li className='flex cursor-pointer' onClick={() => {setEsportsOpen(!esportsOpen); setOpen(false)}}>Esports <span className={esportsOpen ? 'rotate-180 transition-all' : 'transition-all'}>{esportsOpen ? <UilAngleDown color="#00AEFF"/> : <UilAngleDown />}</span></li>
+                    <div id='gradient' className={esportsOpen ? 'absolute top-0 left-0 h-[656px] w-[100%] z-10 flex justify-center items-center flex-col' : 'hidden'}>
+                        <div className='grid grid-cols-5 gap-[100px]'>
+                            {ESPORTS_DROPDOWN.map((data, i) =>(
+                                <div className='flex flex-col items-center gap-[10px] cursor-pointer pb-[80px]'>
+                                    <img src={data.image} alt="" />
+                                    <p className='text-text font-normal text-center text-[14px]'>{data.name}</p>
+                                </div>
+                            ))}
+                        </div>
+                        <div className='w-[100%] h-[90px] bg-dropdown absolute bottom-0 flex items-center justify-center gap-32'>
+                            <li className='flex items-center gap-3 cursor-pointer'><img className='' src={Tournaments} alt="" />Torneios da comunidade</li>
+                        </div>
+                    </div>
+                    <li className='cursor-pointer'>Loja</li>
+                    <li className='cursor-pointer'>Notícias</li>
+                    <li className='cursor-pointer'>Suporte</li>
+        </div>
 
         <div className='absolute top-20 left-0 text-white w-full h-[656px] flex items-center'>
             <div className='ml-52 flex flex-col gap-6 max-lg:ml-28'>
@@ -94,15 +132,14 @@ function Hero() {
             </div>
 
             <div className='absolute mr-52 right-0 flex flex-col gap-28 max-lg:mr-28'>
-                <img src={sliderData.logo} alt="" />
-                <div className='flex flex-col items-end'>
+                <img id='logo' src={sliderData.logo} alt="" />
+                <div id='video' className='flex flex-col items-end'>
                     <p className='text-end uppercase pb-3 font-normal'>Assista o trailer</p>
-                    <div className='w-[300px] h-[180px] bg-blue'>
+                    <div id='vm' className='w-[300px] h-[180px] bg-blue'>
                     
                     </div>
                 </div>
             </div>
-
             <div className='absolute bottom-0 left-0 w-[30%] h-[4px] bg-blue'></div>
         </div>
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,14 +18,11 @@ module.exports = {
       }
     },
     screens: {
-      'sm': '576px',
-      // => @media (min-width: 576px) { ... }
+      'sm': '768px',
 
-      'md': '960px',
-      // => @media (min-width: 960px) { ... }
+      'md': '1320px',
 
       'lg': '1440px',
-      // => @media (min-width: 1440px) { ... }
     },
   },
   plugins: [],
